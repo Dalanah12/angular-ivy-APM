1917638d1e4d77e813524b1371f2671c82608166
Dalanah12 <dalanah12@gmail.com>
2022-06-10 02:34:34 -0400

    2 files modified, 1 added

--- src/app/cities/city-list.component.ts
+++ src/app/cities/city-list.component.ts
@@ -1,7 +1,8 @@
 import { Component } from "@angular/core";
 @Component({
   selector: 'app-cities-list',
-  templateUrl: './city-list.component.html'
+  templateUrl: './city-list.component.html',
+  styleUrls: ['./city-list.component.css']
 })
 export class CityListComponent {
   pageTitle: string = 'Cities';
