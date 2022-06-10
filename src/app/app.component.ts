f387aa8000df24f78ce172ee2394bc7f3df3c3fa
Dalanah12 <dalanah12@gmail.com>
2022-06-10 00:15:23 -0400

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,12 @@ import { Component, VERSION } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  template: `<div><h1>{{pageTitle}}</h1>
+  <app-cities-list></app-cities-list>
+  </div>
+  `
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+  pageTitle: string = 'My Weather App'
 }
