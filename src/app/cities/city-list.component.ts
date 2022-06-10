0d73b0c8ebc9e07daa3b9c9cb4b5b6f98a478daf
Dalanah12 <dalanah12@gmail.com>
2022-06-10 03:18:34 -0400

    2 files modified, 1 added, 1 deleted

--- src/app/cities/city-list.component.ts
+++ src/app/cities/city-list.component.ts
@@ -1,10 +1,14 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 @Component({
   selector: 'app-cities-list',
   templateUrl: './city-list.component.html',
   styleUrls: ['./city-list.component.css']
 })
-export class CityListComponent {
+export class CityListComponent implements OnInit {
   pageTitle: string = 'Cities';
   cities: any[];
+
+  ngOnInit(): void {
+    console.log('In OnInit');
+  }
 }
